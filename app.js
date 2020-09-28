56da4c8835375ffcec0cbcfa6fc399ed7787f4c7
Westley Barragan <barragan@avinc.com>
2020-09-28 10:34:04 -0700

    Added functionality to actually make requests to mailchimp servers using API.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const fs = require('fs');
-
+const https = require("https");
 
 
 const app = express();
@@ -17,11 +17,46 @@ app.post("/", function(req, res){
   console.log(req.body.firstName);
   console.log(req.body.lastName);
   console.log(req.body.emailAddress);
-  fs.readFile("private/apikey.json", (err, data) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const emailAddress = req.body.emailAddress;
+
+  fs.readFile("private/private.json", (err, data) => {
     if (err) throw err;
-    var key = JSON.parse(data.toString()).key;
-    console.log(key);
+    privateData = JSON.parse(data.toString());
+    console.log(privateData.apiKey);
+    console.log(privateData.listId);
+    const subscribeData = {
+      members : [
+        {
+          email_address : emailAddress,
+          status: "subscribed",
+          merge_fields: {
+            FNAME: firstName,
+            LNAME: lastName
+          }
+        }
+      ]
+    };
+    const serverNum = privateData.apiKey[privateData.apiKey.length-1];
+    const jsonData = JSON.stringify(subscribeData);
+    const url = "https://us"+serverNum+".api.mailchimp.com/3.0/lists/"+privateData.listId;
+    const options = {
+      method:"POST",
+      auth: "westley_barragan:" + privateData.apiKey
+    }
+    const request = https.request(url, options, function(response){
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+
+      })
+    });
+
+    request.write(jsonData);
+    request.end();
+
   });
+
 
 });
 app.get("/", function(req, res){
