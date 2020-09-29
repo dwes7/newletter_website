116b4c5980e4cc1cdbb81ca7bbd26e288deb5378
Westley Barragan <barragan@avinc.com>
2020-09-29 10:25:36 -0700

    Added logic to show failure or success html after submission

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const bodyParser = require("body-parser");
 const fs = require('fs');
 const https = require("https");
 
-
 const app = express();
 
 // Used to host static files (e.i. images, html, etc)
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+// main post method when name and email information is submitted
 app.post("/", function(req, res){
   console.log(req.body.firstName);
   console.log(req.body.lastName);
@@ -21,6 +21,10 @@ app.post("/", function(req, res){
   const lastName = req.body.lastName;
   const emailAddress = req.body.emailAddress;
 
+  // Open JSON file with private keys and uids
+  // TODO: figure out how to grab private data without
+  // putting the rest of the code in the the scope of the
+  // read file callback.
   fs.readFile("private/private.json", (err, data) => {
     if (err) throw err;
     privateData = JSON.parse(data.toString());
@@ -38,6 +42,8 @@ app.post("/", function(req, res){
         }
       ]
     };
+
+    // Need server number to contact the correct server. Last character in the API key
     const serverNum = privateData.apiKey[privateData.apiKey.length-1];
     const jsonData = JSON.stringify(subscribeData);
     const url = "https://us"+serverNum+".api.mailchimp.com/3.0/lists/"+privateData.listId;
@@ -45,23 +51,38 @@ app.post("/", function(req, res){
       method:"POST",
       auth: "westley_barragan:" + privateData.apiKey
     }
+
+    // https request
     const request = https.request(url, options, function(response){
+
+      if (response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+      } else{
+        res.sendFile(__dirname + "/failure.html");
+      }
+
+      // get the response from the MailChimp server
       response.on("data", function(data){
         console.log(JSON.parse(data));
-
-      })
+      });
     });
 
     request.write(jsonData);
     request.end();
 
   });
-
-
 });
+
+// main sign up html
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
-app.listen(3000, function(){
-    console.log("Server is listening on port 3000");
+
+app.post("/failure", function(req, res){
+  res.sendFile(__dirname +"/signup.html");
+});
+// run the listening server
+var port = 3000;
+app.listen(port, function(){
+    console.log("Server is listening on port " + port);
 });
